525b8cd6749f74e61c65ca5adad26817e445c9f8
Kunal <kunal@KRP-Pro.fios-router.home>
2018-01-22 20:11:11 -0800

    Removed unnecessary stuff

--- LR/self_regression.py
+++ LR/self_regression.py
@@ -33,13 +33,26 @@ def get_YInter(xs, ys, m):
 def predict(x, m, b):
     return m*x + b
 
+def squared_error(orig,calc):
+    return sum((calc - orig)**2)
+
+def coef_determination(orig, calc):
+    y_mean = [mean(orig) for y in orig]
+    print("Y")
+    squared_error_reg = squared_error(orig, calc)
+    squared_error_mean = squared_error(orig, y_mean)
+    return 1-squared_error_reg/squared_error_mean
+
 m = get_BTSlope(xs, ys)
 b = get_YInter(xs, ys, m)
 
 regression_line = [predict(x,m,b) for x in xs]
 
-print("Slope : ", m)
-print("YInter : ", b)
+
+r = coef_determination(ys, regression_line)
+
+#Coefficient of 1 is the best accuracy
+print("r2 : ", r)
 
 plt.scatter(xs, ys)
 plt.plot(xs, regression_line)
--- Math/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-Linear Algebra : https://www.youtube.com/watch?v=ZK3O402wf1c&list=PLE7DDD91010BC51F8
-Clculus:
-	Single: https://www.youtube.com/watch?v=7K1sB05pE0A&list=PL590CCC2BC5AF3BC1
-	Multi-Variable: https://www.youtube.com/watch?v=PxCxlsl_YwY&list=PL4C4C8A7D06566F38
-
